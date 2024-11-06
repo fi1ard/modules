e3d8143571dcdcd362cb3260c16dd1fc0e4aa652
fi1ard <132204214+fi1ard@users.noreply.github.com>
2024-11-07 01:21:54 +0700

    Update MegaMozg.pydasda
    dasdadasds

--- MegaMozg.py
+++ MegaMozg.py
@@ -1,64 +1,45 @@
-# @KeyZenD & @D4n13l3k00
-
 import random
-import logging
-
 from telethon import types
-from.. import loader, utils
-
-logger = logging.getLogger(__name__)
+from .. import loader, utils
 
 @loader.tds
 class MegaMozgMod(loader.Module):
     strings = {
         'name': 'MegaMozg',
         'pref': '<b>[MegaMozg]</b> ',
-        'need_arg': '{}Нужен аргумент',
-        'tatus': '{}Шанс ответа установлен на 1 к {}',
         'on': '{}Включён',
         'off': '{}Выключен',
-        'no_messages': '{}Не найдено подходящих сообщений для ответа.'
+        'need_arg': '{}Нужен аргумент',
+        'status': '{}{}',
     }
     _db_name = 'MegaMozg'
 
     async def client_ready(self, _, db):
         self.db = db
-    
+
     @staticmethod
     def str2bool(v):
         return v.lower() in ("yes", "y", "ye", "yea", "true", "t", "1", "on", "enable", "start", "run", "go", "да")
 
-    
     async def mozgcmd(self, m: types.Message):
-        '.mozg <on/off/...> - Переключить режим дурачка в чате'
-        args = utils.get_args_raw(m)
+        '.mozg - Включить режим дурачка'
         if not m.chat:
             return
         chat = m.chat.id
-        if self.str2bool(args):
-            chats: list = self.db.get(self._db_name, 'chats', [])
-            chats.append(chat)
-            chats = list(set(chats))
-            self.db.set(self._db_name, 'chats', chats)
-            return await utils.answer(m, self.strings('on').format(self.strings('pref')))
         chats: list = self.db.get(self._db_name, 'chats', [])
-        try:
-            chats.remove(chat)
-        except:
-            pass
-        chats = list(set(chats))
-        self.db.set(self._db_name, 'chats', chats)
-        return await utils.answer(m, self.strings('off').format(self.strings('pref')))
+        if chat not in chats:
+            chats.append(chat)
+            self.db.set(self._db_name, 'chats', chats)
+        return await utils.answer(m, self.strings('on').format(self.strings('pref')))
 
     async def mozgchancecmd(self, m: types.Message):
-        '.mozgchance <int> - Устанвоить шанс 1 к N.\n0 - всегда отвечать'
+        '.mozgchance <int> - Установить шанс 1 к N. 0 - всегда отвечать'
         args: str = utils.get_args_raw(m)
         if args.isdigit():
             self.db.set(self._db_name, 'chance', int(args))
             return await utils.answer(m, self.strings('status').format(self.strings('pref'), args))
-            
         return await utils.answer(m, self.strings('need_arg').format(self.strings('pref')))
-    
+
     async def watcher(self, m: types.Message):
         if not isinstance(m, types.Message):
             return
@@ -66,56 +47,18 @@ class MegaMozgMod(loader.Module):
             return
         if m.chat.id not in self.db.get(self._db_name, 'chats', []):
             return
-        ch = self.db.get(self._db_name, 'chance', 0)
-        if ch!= 0:
-            if random.randint(0, ch)!= 0:
-                return
-        text = m.raw_text
-        words = {random.choice(
-            list(filter(lambda x: len(x) >= 3, text.split()))) for _ in ".."}
-        
-        # Попытка найти сообщения, на которые можно ответить
+
+        # Получаем старые сообщения чата (можно изменить лимит, если нужно больше сообщений)
         msgs = []
-        for word in words:
-            try:
-                async for x in m.client.iter_messages(m.chat.id, search=word):
-                    if x.replies and x.replies.max_id:
-                        msgs.append(x)
-            except Exception as e:
-                logger.warning(f"Ошибка при поиске сообщений: {e}")
-        
-        # Проверка наличия подходящих сообщений
+        async for message in m.client.iter_messages(m.chat.id, limit=50):  # Получаем последние 50 сообщений
+            if message.text:
+                msgs.append(message.text)
+
         if not msgs:
-            await utils.answer(m, self.strings('no_messages').format(self.strings('pref')))
-            return
-        
-        # Выбор случайного сообщения для ответа
-        try:
-            replier = random.choice(msgs)
-        except IndexError:
-            logger.error("Неожиданный IndexError при выборе из непустого списка msgs")
-            return
-        
-        sid = replier.id
-        eid = replier.replies.max_id
-        
-        # Поиск ответов на выбранное сообщение
-        try:
-            msgs_to_reply = [x async for x in m.client.iter_messages(m.chat.id, ids=list(range(sid+1, eid+1))) if x and x.reply_to and x.reply_to.reply_to_msg_id == sid]
-        except Exception as e:
-            logger.warning(f"Ошибка при поиске ответов: {e}")
-            return
-        
-        # Проверка наличия ответов
-        if not msgs_to_reply:
-            await utils.answer(m, self.strings('no_messages').format(self.strings('pref')))
-            return
-        
-        # Ответ на случайный ответ
-        try:
-            msg_to_send = random.choice(msgs_to_reply)
-            await m.reply(msg_to_send)
-        except IndexError:
-            logger.error("Неожиданный IndexError при выборе из непустого списка msgs_to_reply")
-        except Exception as e:
-            logger.error(f"Ошибка при отправке ответа: {e}")
+            return  # Если сообщений нет, ничего не отправляем
+
+        # Выбираем случайное сообщение из истории чата
+        random_message = random.choice(msgs)
+
+        # Отправляем выбранное сообщение в ответ
+        await m.reply(random_message)
