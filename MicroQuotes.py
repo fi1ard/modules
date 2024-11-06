27faa26cecc440b16bbd180386b60362c3c030d4
fi1ard <132204214+fi1ard@users.noreply.github.com>
2024-11-07 01:32:16 +0700

    Update MicroQuotes.pyвыф
    вфывфы

--- MicroQuotes.py
+++ MicroQuotes.py
@@ -1,51 +1,64 @@
+import random
+from telethon import types
 from .. import loader, utils
-import io
-from PIL import Image, ImageFont, ImageDraw
-import requests
-import textwrap
 
 @loader.tds
-class MicroQuotesMod(loader.Module):
-	"""Микроцитаты"""
-	strings = {"name": "MicroQuotes"}
-	
-	async def mqcmd(self, message):
-		""".mq <реплай на текст>"""
-		bw = False if utils.get_args(message) else True
-		reply = await message.get_reply_message()
-		if not reply or not reply.raw_text:
-			await message.edit("<b>Ответь командой на умную цитату!</b>")
-			return
-		sender = reply.sender_id
-		
-		if not sender:
-			sender = message.chat.id
-		if sender == 1087968824:
-			sender = message.chat.id
-		pfp = await message.client.download_profile_photo(sender, bytes)
-		await message.edit("<i>И сказал этот гений...</i>")
-		if not pfp:
-			pfp = b'BM:\x00\x00\x00\x00\x00\x00\x006\x00\x00\x00(\x00\x00\x00\x01\x00\x00\x00\x01\x00\x00\x00\x01\x00\x18\x00\x00\x00\x00\x00\x04\x00\x00\x00\xc4\x0e\x00\x00\xc4\x0e\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xff\xff\xff\x00'
-		text = "\n".join(textwrap.wrap(reply.raw_text, 30))
-		text = "“"+text+"„"
-		bf = requests.get("https://raw.githubusercontent.com/KeyZenD/l/master/times.ttf").content
-		font = ImageFont.truetype(io.BytesIO(bf), 50)
-		im = Image.open(io.BytesIO(pfp))
-		if bw:
-			im = im.convert("L")
-		im = im.convert("RGBA").resize((1024, 1024))
-		w, h = im.size
-		w_, h_ = 20*(w//100), 20*(h//100)
-		im_ = Image.new("RGBA", (w-w_, h-h_), (0, 0, 0))
-		im_.putalpha(150) 
-		im.paste(im_, (w_//2, h_//2), im_)
-		draw = ImageDraw.Draw(im)
-		_w, _h = draw.textsize(text=text, font=font)
-		x, y = (w-_w)//2, (h-_h)//2
-		draw.text((x, y), text=text, font=font, fill="#fff", align="center")
-		output=io.BytesIO()
-		im.save(output, "PNG")
-		output.seek(0)
-		await reply.reply(file=output)
-		await message.delete()
-		
+class MegaMozgMod(loader.Module):
+    strings = {
+        'name': 'MegaMozg',
+        'pref': '<b>[MegaMozg]</b> ',
+        'on': '{}Включён',
+        'off': '{}Выключен',
+        'need_arg': '{}Нужен аргумент',
+        'status': '{}{}',
+    }
+    _db_name = 'MegaMozg'
+
+    async def client_ready(self, _, db):
+        self.db = db
+
+    @staticmethod
+    def str2bool(v):
+        return v.lower() in ("yes", "y", "ye", "yea", "true", "t", "1", "on", "enable", "start", "run", "go", "да")
+
+    async def mozgcmd(self, m: types.Message):
+        '.mozg - Включить режим дурачка'
+        if not m.chat:
+            return
+        chat = m.chat.id
+        chats: list = self.db.get(self._db_name, 'chats', [])
+        if chat not in chats:
+            chats.append(chat)
+            self.db.set(self._db_name, 'chats', chats)
+        return await utils.answer(m, self.strings('on').format(self.strings('pref')))
+
+    async def mozgchancecmd(self, m: types.Message):
+        '.mozgchance <int> - Установить шанс 1 к N. 0 - всегда отвечать'
+        args: str = utils.get_args_raw(m)
+        if args.isdigit():
+            self.db.set(self._db_name, 'chance', int(args))
+            return await utils.answer(m, self.strings('status').format(self.strings('pref'), args))
+        return await utils.answer(m, self.strings('need_arg').format(self.strings('pref')))
+
+    async def watcher(self, m: types.Message):
+        if not isinstance(m, types.Message):
+            return
+        if m.sender_id == (await m.client.get_me()).id or not m.chat:
+            return
+        if m.chat.id not in self.db.get(self._db_name, 'chats', []):
+            return
+
+        # Получаем старые сообщения чата (можно изменить лимит, если нужно больше сообщений)
+        msgs = []
+        async for message in m.client.iter_messages(m.chat.id, limit=50):  # Получаем последние 50 сообщений
+            if message.text:  # Проверка на None
+                msgs.append(message.text)
+
+        if not msgs:
+            return  # Если сообщений нет, ничего не отправляем
+
+        # Выбираем случайное сообщение из истории чата
+        random_message = random.choice(msgs)
+
+        # Отправляем выбранное сообщение в ответ
+        await m.reply(random_message)
